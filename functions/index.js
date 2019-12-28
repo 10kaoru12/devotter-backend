d768c4c6cd2c976dd3fa3eaa9ea7b5f34e0cb03a
Kaoru Ishizawa <kaoru.ishizawa@nnlife.co.jp>
2019-12-29 00:30:32 +0900

    cloud functions connect firestore

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('./atcontributter-firebase-adminsdk-5p86i-6e55085ef8.json');
+const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+adminConfig.credential = admin.credential.cert(serviceAccount);
+admin.initializeApp(adminConfig);
+const firestore = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+    firestore.collection("users").get()
+        // eslint-disable-next-line promise/always-return
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                fetchedData = doc.id;
+                console.log("data" + `${doc.id} => ${doc.data()}`);
+            });
+            response.send("data " + fetchedData);
+        }).catch((error) => {
+            response.send("data" + error);
+        });
 });
