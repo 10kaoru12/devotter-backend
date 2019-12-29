4697a0e1adc15b5939c9ef1a3f41cc955a0f8a5e
Kaoru Ishizawa <kaoru.ishizawa@nnlife.co.jp>
2019-12-29 16:00:23 +0900

    cloud functions connect cloud storage

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,53 @@
 const functions = require('firebase-functions');
+
+// const axios = require('axios');
+
 const admin = require('firebase-admin');
+
 const serviceAccount = require('./atcontributter-firebase-adminsdk-5p86i-6e55085ef8.json');
 const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 adminConfig.credential = admin.credential.cert(serviceAccount);
 admin.initializeApp(adminConfig);
 const firestore = admin.firestore();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    firestore.collection("users").get()
-        // eslint-disable-next-line promise/always-return
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                fetchedData = doc.id;
-                console.log("data" + `${doc.id} => ${doc.data()}`);
-            });
-            response.send("data " + fetchedData);
-        }).catch((error) => {
-            response.send("data" + error);
+const bucket = admin.storage().bucket();
+
+exports.devotterCronJob = functions.https.onRequest((request, response) => {
+    firestore.collection("users").doc("kaoru1012").get()
+        .then(querySnapShot => {
+            response.send("hello");
+            console.log(querySnapShot.data().accessTokenSecret);
+            console.log(querySnapShot.data().accessToken);
+            return 0;
+        })
+        .catch(() => {
+            response.send("firestore access promise disconnecting error!");
+            return 0;
         });
+
+    bucket.file("ac.json").download()
+        .then((result) => {
+            let receiveAc = JSON.parse(result);
+            console.log(receiveAc);
+            let senderAc = JSON.stringify(receiveAc);
+            let uploadPath = "generate.json";
+            // eslint-disable-next-line promise/no-nesting
+            bucket.file(uploadPath).save(senderAc);
+            return 0;
+        }).catch(() => {
+            console.log("error");
+            return
+        });
+
+    // firestore.collection("users").get()
+    //     // eslint-disable-next-line promise/always-return
+    //     .then((querySnapshot) => {
+    //         querySnapshot.forEach((doc) => {
+    //             fetchedData = doc.id;
+    //             console.log("data" + `${doc.id} => ${doc.data()}`);
+    //         });
+    //         response.send("data " + fetchedData);
+    //     }).catch((error) => {
+    //         response.send("data" + error);
+    //     });
 });
