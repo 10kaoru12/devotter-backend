f7ce1a3bce0ce9b550b43fd7c01ddea4ec5e2e4b
Kaoru Ishizawa <kaoru.ishizawa@nnlife.co.jp>
2019-12-29 22:17:33 +0900

    add comment.

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,50 @@
+//import
 const functions = require('firebase-functions');
-
 const axios = require('axios');
-
 const admin = require('firebase-admin');
-
 const serviceAccount = require('./atcontributter-firebase-adminsdk-5p86i-6e55085ef8.json');
+
+//initialize
 const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 adminConfig.credential = admin.credential.cert(serviceAccount);
 admin.initializeApp(adminConfig);
-const firestore = admin.firestore();
 
+//create instance
+const firestore = admin.firestore();
 const bucket = admin.storage().bucket();
 
-exports.devotterCronJob = functions.https.onRequest((request, response) => {
-    // firestore.collection("users").doc("kaoru1012").get()
-    //     .then(querySnapShot => {
-    //         response.send("hello");
-    //         console.log(querySnapShot.data().accessTokenSecret);
-    //         console.log(querySnapShot.data().accessToken);
-    //         return 0;
-    //     })
-    //     .catch(() => {
-    //         response.send("firestore access promise disconnecting error!");
-    //         return 0;
-    //     });
+//global
+let consumerKey;
+let consumerKeySecret;
+let accessToken;
+let accessTokenSecret;
 
+//main
+exports.devotterCronJob = functions.https.onRequest((request, response) => {
+    firestore.collection("users").doc("kaoru1012").get()
+        .then(querySnapShot => {
+            accessToken = querySnapShot.data().accessToken;
+            accessTokenSecret = querySnapShot.data().accessTokenSecret;
+            console.log(accessToken);
+            console.log(accessTokenSecret);
+            return 0;
+        })
+        .catch(() => {
+            response.status(200).send("Access Promise for collection users failed.");
+            return 0;
+        });
+    firestore.collection("api").doc("keys").get()
+        .then(querySnapShot => {
+            consumerKey = querySnapShot.data().consumerKey;
+            consumerKeySecret = querySnapShot.data().consumerKeySecret;
+            console.log(consumerKey);
+            console.log(consumerKeySecret);
+            return 0;
+        })
+        .catch(() => {
+            response.status(200).send("Access Promise for collection api failed.");
+            return 0;
+        });
     bucket.file("ac.json").download()
         .then((result) => {
             let receiveAc = JSON.parse(result);
@@ -38,24 +58,11 @@ exports.devotterCronJob = functions.https.onRequest((request, response) => {
             }
             // let senderAc = JSON.stringify(receiveAc);
             // let uploadPath = "generate.json";
-            // // eslint-disable-next-line promise/no-nesting
             // bucket.file(uploadPath).save(senderAc);
             return 0;
-        }).catch((error) => {
-            console.log(error);
-            console.log("error");
+        })
+        .catch(() => {
+            response.status(200).send("The download promise of firestore failed.");
             return 0;
         });
-
-    // firestore.collection("users").get()
-    //     // eslint-disable-next-line promise/always-return
-    //     .then((querySnapshot) => {
-    //         querySnapshot.forEach((doc) => {
-    //             fetchedData = doc.id;
-    //             console.log("data" + `${doc.id} => ${doc.data()}`);
-    //         });
-    //         response.send("data " + fetchedData);
-    //     }).catch((error) => {
-    //         response.send("data" + error);
-    //     });
 });
